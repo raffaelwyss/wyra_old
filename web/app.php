63642562172fca2f911f1d1ee1403fe796221a80
Raffael Wyss <raffael.wyss@gmail.com>
2016-10-13 16:46:25 +0200

    AppKernel begonnen zu implementieren

---
+++ web/app.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = __DIR__.'/../vendor/autoload.php';
+
+
+
+echo 'Fertig mit dem Laden von WyRa';
--- web/index.php
+++ web/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo 'WyRa';
+header("Location: /app.php");
