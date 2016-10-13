40ce418911b8294a42bbf3653a4b3d0d3d1da78a
Raffael <raffael.wyss@gmail.com>
2016-10-13 22:20:54 +0200

    Einrichtung der Konfiguration für Symfony (Zwischencommit)

--- web/app.php
+++ web/app.php
@@ -1,10 +1,22 @@
 <?php
 
-
+use WyRa\AppKernel;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
+require '../app/AppKernel.php';
 
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
 
+$kernel = new AppCache($kernel);
+
+Request::enableHttpMethodParameterOverride();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
 
 echo 'Fertig mit dem Laden von WyRa';
